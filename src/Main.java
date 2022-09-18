741d22a30ca8e541b07c8500acbd8ddf1a784ab3
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-18 23:08:37 +0500

    You're given a representation of a 5x5 2D map, and if you can only move left, right, up, or down, determine how many moves you would have to make to get between two points on the map.
    Task:
     Determine the total number of moves that are needed between two points on a map.  The points that you move between are marked with a P and the spaces in between are marked with X.
    
    Input Format:
    A string that represents the 2D space starting at the top left.  Each level from top to bottom is separated by a comma.
    
    Output Format:
    An integer that represents the total number of moves that you had to make.
    
    Sample Input:
    XPXXX,XXXXX,XXXXX,XXXPX,XXXXX
    
    Sample Output:
    5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String map = sc.nextLine();
+        int p1 = map.indexOf('P');
+        int p2 = map.indexOf('P', p1+1);
+        int w = Math.abs(p1/6 - p2/6);
+        int h = Math.abs(p1%6 - p2%6);
+        System.out.println(w+h);
     }
 }
